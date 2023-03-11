5bbc474336749bd144f7e6fbd1b6f7bf8898b83e
onrlcn-ff <onurlacin111@outlook.com>
2023-03-11 15:00:53 +0300

    Add Admin Panel Tag and Action

---
+++ Controllers/AdminTagController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogie.Web.Controllers
+{
+    public class AdminTagController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmmitTag()
+        {
+            var name = Request.Form["name"];
+            var displayName= Request.Form["displayName"];
+            return View("Add");
+
+        }
+    }
+}

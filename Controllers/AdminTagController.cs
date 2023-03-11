4477ead62dd5cf6a4628bf7d1646c3da50de1351
onrlcn-ff <onurlacin111@outlook.com>
2023-03-11 16:51:59 +0300

    List Tag Page and Controllers

--- Controllers/AdminTagController.cs
+++ Controllers/AdminTagController.cs
@@ -1,10 +1,17 @@
-﻿using Blogie.Web.Models.ViewModels;
+﻿using Blogie.Web.Data;
+using Blogie.Web.Models.Domain;
+using Blogie.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blogie.Web.Controllers
 {
     public class AdminTagController : Controller
     {
+        private BlogieDBContext _blogieDbContext;
+        public AdminTagController(BlogieDBContext blogieDBContext)
+        {
+            _blogieDbContext = blogieDBContext;
+        }
         [HttpGet]
         public IActionResult Add()
         {
@@ -18,10 +25,22 @@ namespace Blogie.Web.Controllers
             //var name = Request.Form["name"];
             //var displayName= Request.Form["displayName"];
 
-            var name = addTagRequest.Name;
-            var displayName = addTagRequest.DisplayName;
+            //var name = addTagRequest.Name;
+            //var displayName = addTagRequest.DisplayName;
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+            _blogieDbContext.Add(tag);
+            _blogieDbContext.SaveChanges();
             return View("Add");
-
+        }
+        [HttpGet]
+        public ActionResult List()
+        {
+            var tags = _blogieDbContext.Tags.ToList();
+            return View(tags);
         }
     }
 }
